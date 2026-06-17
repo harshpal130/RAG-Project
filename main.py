c8482047ff47712928c17c52fb9b17613d783e29
Harsh Pal <harshpal130444@gmail.com>
2026-06-17 23:19:28 +0530

    Refactor main.py to integrate Chroma vector store and enhance prompt handling

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
 from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-from langchain_community.document_loaders import TextLoader
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+) 
 
-
-data = PyPDFLoader("documents loaders/deeplearning.pdf")
-docs = data.load()
-
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap= 200
+vectorStore = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embedding_model
 )
 
-chunk = splitter.split_documents(docs)
+retriver = vectorStore.as_retriever(
+    search_type ="mmr",
+    search_kwargs={
+        "k":4,
+        "fetch_k":10,
+        "lambda_mult":0.5,  #0 for unrelated answer and 1 for releated answer
 
-template= ChatPromptTemplate.from_messages([
-    ("system", "you are a Ai that summaries the text"),
-    ("human","{data}")
-])
+    }
+)
 
-model = ChatMistralAI(model="mistral-small-2603")
-prompt = template.format_messages(data= docs)
+llm = ChatMistralAI(model="mistral-small-2506")
 
-response = model.invoke(prompt)
+# prompt template
 
-print(response.content)
+promt = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+         """You are helpful AI agent ..
+         use ONLY provided context  to answer the question.
+         if answer is not present in the context,
+         say:I could not find the answer in the document 
+         """),
+         ("human",
+          
+          """Context:{context}.
+          Question :{question}.
+          """
+          
+          )
+    ]
+)
+
+print("rag system created ")
+
+print("press 0 to exit ")
+
+while True:
+    query = input("You:")
+    if query == "0":
+        break
+
+    docs = retriver.invoke(query)
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )  
+
+    final_prompt = promt.invoke({
+        "context":context,
+        "question":query
+    })  
+
+    response = llm.invoke(final_prompt)
+    print(f"\n AI: {response.content}")
